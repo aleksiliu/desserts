0fa885e09736758a15542909826364f401572db8
Aleksi <liukkonenaleksi@gmail.com>
2024-08-19 09:57:31 +0300

    Add tailwind config and few components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,29 @@ export default {
 	theme: {
 		container: {
 			center: true,
+			padding: "1rem",
+			screens: {
+			  sm: "100%",
+			  md: "100%",
+			  lg: "1024px",
+			  xl: "1440px",
+			},
+		  },
+		  screens: {
+			'2xl': {'max': '1535px'},
+			// => @media (max-width: 1535px) { ... }
+	  
+			'xl': {'max': '1279px'},
+			// => @media (max-width: 1279px) { ... }
+	  
+			'lg': {'max': '1023px'},
+			// => @media (max-width: 1023px) { ... }
+	  
+			'md': {'max': '767px'},
+			// => @media (max-width: 767px) { ... }
+	  
+			'sm': {'max': '639px'},
+			// => @media (max-width: 639px) { ... }
 		  },
 		extend: {},
 	},
