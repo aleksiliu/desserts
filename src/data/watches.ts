aa78b3d9e76fe5fa7c166b72e30935be089faee5
Aleksi <liukkonenaleksi@gmail.com>
2024-08-19 18:18:13 +0300

    Remove images

--- src/data/watches.ts
+++ src/data/watches.ts
@@ -2,55 +2,55 @@ import type { Watch } from './types';
 
 export const watchItems: Watch[] = [
     {
-        "image": "./assets/images/image-rolex-submariner-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Rolex Submariner",
         "brand": "Rolex",
         "price": 8500
     },
     {
-        "image": "./assets/images/image-omega-seamaster-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Omega Seamaster",
         "brand": "Omega",
         "price": 5000
     },
     {
-        "image": "./assets/images/image-tag-heuer-carrera-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Tag Heuer Carrera",
         "brand": "Tag Heuer",
         "price": 3500
     },
     {
-        "image": "./assets/images/image-cartier-santos-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Cartier Santos",
         "brand": "Cartier",
         "price": 7000
     },
     {
-        "image": "./assets/images/image-patek-philippe-nautilus-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Patek Philippe Nautilus",
         "brand": "Patek Philippe",
         "price": 30000
     },
     {
-        "image": "./assets/images/image-breitling-navitimer-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Breitling Navitimer",
         "brand": "Breitling",
         "price": 6000
     },
     {
-        "image": "./assets/images/image-hublot-big-bang-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Hublot Big Bang",
         "brand": "Hublot",
         "price": 12000
     },
     {
-        "image": "./assets/images/image-audemars-piguet-royal-oak-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Audemars Piguet Royal Oak",
         "brand": "Audemars Piguet",
         "price": 25000
     },
     {
-        "image": "./assets/images/image-zenith-chronomaster-desktop.jpg",
+        "image": "./assets/images/image-rolex.webp",
         "name": "Zenith Chronomaster",
         "brand": "Zenith",
         "price": 8000
